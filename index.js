5adb8abee4026fd60c2c14dbf5d290b4bebea585
unknown <user.name>
2024-02-15 15:17:33 -0300

    Estrutura condicionais

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const app = express();
 //configurando expres para view engine
 app.set("view engine", 'ejs')
 
-app.get("/", (req, res) => {
-    res.render("home")
+app.get("/:nome/:lang", (req, res) => {
+    var nome = req.params.nome
+    var lang = req.params.lang
+    var exibirMSG = false
+    res.render("home", {
+        nome: nome,
+        lang: lang,
+        empresa: "Guia do programador",
+        inscritos: 8000,
+        msg: exibirMSG
+    })
+
 });
 
 app.listen(8080,()=>{
