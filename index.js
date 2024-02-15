6e86709fc6290766a0de711e21bb6989980676fa
unknown <user.name>
2024-02-15 16:47:39 -0300

    Adicionando partials

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ app.set("view engine", 'ejs')
 app.use(express.static('public')); //definindo arquivos staticos (Arquivos de imagens css e etc)
 
 
-app.get("/:nome/:lang", (req, res) => {
+app.get("/", (req, res) => {
     res.render("home")
 });
 
 app.get("/perguntar", (req, res) => {
     res.render("perguntar")
+});
+
+app.post("/salvarpergunta", (req, res)=> {
+    
+    res.send("Formulario enviado");
 });
 
 app.listen(8080,()=>{
