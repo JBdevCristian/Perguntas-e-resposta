eabf922590e78fed03642d66be86339b487181fd
unknown <user.name>
2024-02-15 20:14:24 -0300

    Adicionando MYSQL

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser")
+const connection = require('./database/database') //exportando database
+
+//database
+connection
+    .authenticate()
+    .then(()=>{
+        console.log("Conexão feita com sucesso");
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
 
 //configurando expres para view engine
 app.set("view engine", 'ejs')
 app.use(express.static('public')); //definindo arquivos staticos (Arquivos de imagens css e etc)
 
+//bodyParser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+//rotas
 app.get("/", (req, res) => {
     res.render("home")
 });
@@ -15,8 +31,11 @@ app.get("/perguntar", (req, res) => {
 });
 
 app.post("/salvarpergunta", (req, res)=> {
-    
-    res.send("Formulario enviado");
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+
+    res.send("Formulario enviado titulo " + titulo + " " + "Descrição " + descricao);
 });
 
 app.listen(8080,()=>{
