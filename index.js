a4a1d5b2c9165bef5e8a15303d3857eb112a2fea
unknown <user.name>
2024-02-15 15:50:39 -0300

    Adicionando arquivos static

--- index.js
+++ index.js
@@ -3,17 +3,28 @@ const app = express();
 
 //configurando expres para view engine
 app.set("view engine", 'ejs')
+app.use(express.static('public')); //definindo arquivos staticos (Arquivos de imagens css e etc)
+
 
 app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome
     var lang = req.params.lang
     var exibirMSG = false
+
+    var produtos = [
+        {nome: "Doritos", preco: 3.14},
+        {nome: "Coca", preco: 5},
+        {nome: "Leite", preco: 1.45}
+
+    ]
+
     res.render("home", {
         nome: nome,
         lang: lang,
         empresa: "Guia do programador",
         inscritos: 8000,
-        msg: exibirMSG
+        msg: exibirMSG,
+        produtos: produtos
     })
 
 });
